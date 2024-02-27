65efd088a19b13e71521f56a445ef301905a710f
Victor <soujvnunes@gmail.com>
2024-02-27 20:52:05 -0300

    feat(vite.config): config plugins
    vite-plugin-webfont-dl

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload()],
+  plugins: [react(), webfontDownload(["https://use.typekit.net/bwb4cjf.css"])],
   resolve: {
     alias: [
       {
