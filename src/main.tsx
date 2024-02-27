f255004f51676dd3a02e0704cba285ee748ec917
Victor <soujvnunes@gmail.com>
2024-02-27 20:39:43 -0300

    feat(main.tsx): adjust with tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./main.css";
 
 const container = document.getElementById("root");
 const root = container ? createRoot(container) : null;
