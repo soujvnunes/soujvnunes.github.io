5de81934bdebb342e6adbaa142b27ba954db30d4
Victor <soujvnunes@gmail.com>
2024-02-27 20:48:48 -0300

    feat(vite.config): install plugins
    vite-plugin-webfont-dl

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), webfontDownload()],
   resolve: {
     alias: [
       {
