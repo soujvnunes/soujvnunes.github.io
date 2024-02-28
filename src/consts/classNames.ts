4153a690902537dbed647ed60c6b48489e5f3c93
Victor <soujvnunes@gmail.com>
2024-02-28 20:51:30 -0300

    feat(classNames): add button

--- src/consts/classNames.ts
+++ src/consts/classNames.ts
@@ -2,6 +2,9 @@ const classNames = {
   container: {
     root: "pl-4 pr-4 lg:pl-8 lg:pr-8",
   },
+  button: {
+    root: "inline-flex items-center h-10 pl-2 pr-2 rounded-lg hover:bg-black/20",
+  },
 };
 
 export default classNames;
