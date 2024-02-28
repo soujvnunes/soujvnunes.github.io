e9501e57c8f1efeafeefbee9c2aaaa0fe011cab6
Victor <soujvnunes@gmail.com>
2024-02-28 15:52:25 -0300

    feat(consts): add socials and projects links

---
+++ src/shared/consts.ts
@@ -0,0 +1,45 @@
+export const SOCIALS = [
+  {
+    href: "https://github.com/soujvnunes",
+    title: "Github",
+  },
+  {
+    href: "https://www.linkedin.com/in/soujvnunes/",
+    title: "LinkedIn",
+  },
+  {
+    href: "https://medium.com/@soujvnunes",
+    title: "Medium",
+  },
+  {
+    href: "https://www.behance.net/soujvnunes",
+    title: "Behance",
+  },
+];
+
+export const PROJECTS = [
+  {
+    id: "1",
+    href: "https://app.polkamarkets.com/",
+    title: "App Polkamarkets",
+    color: "bg-blue-100",
+    description:
+      "Design review and implementation of UI/UX to be integrated with MetaMask interface or other digital wallets services.",
+  },
+  {
+    id: "2",
+    href: "https://sites.ufal.br/ufal-60-anos/",
+    title: "UFAL 60 Anos",
+    color: "bg-red-100",
+    description:
+      "Design and implementation of a virtual exhibition that shows the evolution of the Federal University of Alagoas.",
+  },
+  {
+    id: "3",
+    href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
+    title: "Ways to create Design Tokens",
+    color: "bg-neutral-100",
+    description:
+      "Writing of step-by-step on how to create design tokens based on an already developed web application and without documentation.",
+  },
+];
