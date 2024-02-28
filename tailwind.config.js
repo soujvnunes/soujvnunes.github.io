0bacf23d3a99c1c78491d72fd4e115c386097323
Victor <soujvnunes@gmail.com>
2024-02-28 14:51:51 -0300

    feat(tailwind.config): adjust Freight Display Pro

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['"sofia-pro"', ...defaultTheme.fontFamily.sans],
+        serif: ['"freight-display-pro"', ...defaultTheme.fontFamily.serif],
       },
     },
   },
