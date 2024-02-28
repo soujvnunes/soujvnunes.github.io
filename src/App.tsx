b876d414c76b6b1a0c8046c2e025bf8d25700077
Victor <soujvnunes@gmail.com>
2024-02-27 21:01:01 -0300

    feat(App.tsx): remove default content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SOCIALS } from "@shared/consts";
+import me from "/me.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      In construction 🔨🔨
+      {/* <header className="relative text-center">
+        <h1 className="mb-4 text-2xl font-light text-blue-200 lg:mb-6 lg:text-5xl">
+          <img
+            src={me}
+            className="w-10 h-10 rounded-tl-lg rounded-bl-lg shadow-lg lg:h-16 lg:w-16 shadow-indigo-950"
+            alt="Vite logo"
+          />
+          <span className="text-xs font-medium text-left text-blue-300">
+            Hi, I&apos;m Victor
+            <small className="block font-mono uppercase">Open to work</small>
+          </span>
+        </h1>
+      </header>
+      <main className="m-auto max-md:p-4">
+        <section>
+          <h2 className="text-8xl font-bold">
+            Lemme build you something extraordinary!
+          </h2>
+        </section>
+        <section className="space-y-4 card">
+          <p className="text-justify">
+            Hi, I&apos;m Victor, a Brazilian Frontend/Design Engineer, but as a
+            developer, I&apos;m a citizen of the world. It all started when I
+            graduated with an I.T Technician degree in 2014.
+          </p>
+          <p className="text-justify">
+            Since then, I have worked as a service provider with plenty of
+            excitement to build even more. I&apos;ve been dealing directly with
+            I.T and product leaders to deliver extraordinary and complex UIs,
+            from crypto transaction flows to beautiful landing product pages.
+          </p>
+          <p className="text-justify text-white/80">
+            My passion while developing is focused on design standards, such as
+            accessibility and usability across web and app interfaces, in which
+            I have a great taste at the same time I deliver universal access for
+            a product.
+          </p>
+        </section>
+        <footer>
+          <ul>
+            {SOCIALS.map((social) => (
+              <li key={social.title}>
+                <a href={social.href}>{social.iconName}</a>
+              </li>
+            ))}
+          </ul>
+        </footer>
+      </main> */}
     </>
-  )
+  );
 }
-
-export default App
