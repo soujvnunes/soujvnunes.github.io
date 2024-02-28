b376d95d0b3a596c61063ba25c8a683afdc22d4f
Victor <soujvnunes@gmail.com>
2024-02-28 14:51:34 -0300

    feat(vite.config): add material symbols

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload(["https://use.typekit.net/bwb4cjf.css"])],
+  plugins: [
+    react(),
+    webfontDownload([
+      "https://use.typekit.net/bwb4cjf.css",
+      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+    ]),
+  ],
   resolve: {
     alias: [
       {
