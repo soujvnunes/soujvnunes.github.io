7028dce2dc71e55d277e8c48aaaf10ef259cdc9d
Victor <soujvnunes@gmail.com>
2024-02-28 20:48:07 -0300

    feat(vite.config): config paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import webfontDownload from "vite-plugin-webfont-dl";
-import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,8 +15,16 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: "@shared",
-        replacement: "/src/shared",
+        find: "consts",
+        replacement: "/src/consts",
+      },
+      {
+        find: "helpers",
+        replacement: "/src/helpers",
+      },
+      {
+        find: "components",
+        replacement: "/src/components",
       },
     ],
   },
