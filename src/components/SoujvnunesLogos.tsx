1a19cc525aa37e854213d14362fc8f8a507f1543
Victor <soujvnunes@gmail.com>
2024-02-28 20:47:29 -0300

    feat(SoujvnunesLogos): compose

---
+++ src/components/SoujvnunesLogos.tsx
@@ -0,0 +1,23 @@
+export default function SoujvnunesLogos() {
+  return (
+    <a href="/" className="mr-auto">
+      <h1>
+        <strong>
+          sou
+          <span className="text-red-600">j</span>
+          <span className="text-amber-600">v</span>
+          <span className="text-blue-600">n</span>
+          unes
+        </strong>
+        <span
+          aria-hidden
+          className="-ml-1.5 -mr-1.5 align-middle material-symbols-outlined"
+        >
+          pen_size_1
+        </span>
+        design_
+        <strong>engineer</strong>
+      </h1>
+    </a>
+  );
+}
