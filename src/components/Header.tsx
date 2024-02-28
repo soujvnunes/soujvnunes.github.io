3c9f35b2ad17a42911504a18b4229175b4b92eb2
Victor <soujvnunes@gmail.com>
2024-02-28 20:53:05 -0300

    feat(Header): compose

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import classNames from "consts/classNames";
+import cn from "helpers/cn";
+import SoujvnunesLogos from "./SoujvnunesLogos";
+import ThemeButton from "./ThemeButton";
+
+export default function Header() {
+  return (
+    <header
+      className={cn(
+        "flex items-center h-16 bg-white",
+        classNames.container.root,
+      )}
+    >
+      <SoujvnunesLogos />
+      <ThemeButton />
+    </header>
+  );
+}
