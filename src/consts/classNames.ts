a471d19270e9d813558d468dc0d2b129833e655a
Victor <soujvnunes@gmail.com>
2024-02-28 21:26:58 -0300

    feat(classNames): add text and surface

--- src/consts/classNames.ts
+++ src/consts/classNames.ts
@@ -3,7 +3,13 @@ const classNames = {
     root: "pl-4 pr-4 lg:pl-8 lg:pr-8",
   },
   button: {
-    root: "inline-flex items-center h-10 pl-2 pr-2 rounded-lg hover:bg-black/20",
+    root: "inline-flex items-center h-10 pl-2 pr-2 rounded-lg hover:bg-black/20 dark:hover:bg-white/20",
+  },
+  text: {
+    secondary: "text-black/60 dark:text-white/80",
+  },
+  surface: {
+    primary: "bg-white dark:bg-gray-900",
   },
 };
 
