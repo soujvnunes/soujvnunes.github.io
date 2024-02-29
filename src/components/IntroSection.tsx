6f1c6752cfac920351933c95fa9b98cf5a8c7f10
Victor <soujvnunes@gmail.com>
2024-02-28 21:27:16 -0300

    feat(IntroSection): adjust dark mode

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -9,7 +9,7 @@ export default function IntroSection() {
         classNames.container.root,
       )}
     >
-      <p className="inline-block ml-1 lg:ml-2 text-black/60">
+      <p className={cn("inline-block ml-1 lg:ml-2", classNames.text.secondary)}>
         <img
           src="/me.jpg"
           className="inline-block w-10 h-10 lg:w-16 lg:h-16"
@@ -22,7 +22,12 @@ export default function IntroSection() {
       </h2>
       <hr className="sr-only" />
       <p>
-        <button className="font-serif italic lg:text-2xl text-black/60">
+        <button
+          className={cn(
+            "font-serif italic lg:text-2xl",
+            classNames.text.secondary,
+          )}
+        >
           More about me
           <span className="ml-1 text-4xl align-middle material-symbols-outlined wght-200">
             trending_flat
