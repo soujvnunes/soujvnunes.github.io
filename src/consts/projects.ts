599e7118df62e9fcf1ddb803d2b543d379b3433e
Victor <soujvnunes@gmail.com>
2024-02-28 21:26:37 -0300

    feat(projects): adjust dark mode

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -3,7 +3,7 @@ const PROJECTS = [
     id: "1",
     href: "https://app.polkamarkets.com/",
     title: "App Polkamarkets",
-    color: "bg-blue-100",
+    color: "bg-blue-100 dark:bg-blue-500/10",
     description:
       "Design review and implementation of UI/UX to be integrated with MetaMask interface or other digital wallets services.",
   },
@@ -11,7 +11,7 @@ const PROJECTS = [
     id: "2",
     href: "https://sites.ufal.br/ufal-60-anos/",
     title: "UFAL 60 Anos",
-    color: "bg-red-100",
+    color: "bg-red-100 dark:bg-red-500/10",
     description:
       "Design and implementation of a virtual exhibition that shows the evolution of the Federal University of Alagoas.",
   },
@@ -19,7 +19,7 @@ const PROJECTS = [
     id: "3",
     href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
     title: "Ways to create Design Tokens",
-    color: "bg-neutral-100",
+    color: "bg-neutral-100 dark:bg-white/10",
     description:
       "Writing of step-by-step on how to create design tokens based on shipped web application and without documentation.",
   },
