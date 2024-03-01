ba629ae6e16d9c5bb0dfb0e64783a2bbada5f08d
Victor <soujvnunes@gmail.com>
2024-03-01 10:52:43 -0300

    feat(ThemeButton): optical alignment

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,10 +1,11 @@
 import classNames from "consts/classNames";
+import cn from "helpers/cn";
 
 export default function ThemeButton() {
   // todo: implement this URGENTLY
   return (
     <button
-      className={classNames.button.root}
+      className={cn(classNames.button.root, "-mr-2")}
       aria-label="Change to dark mode"
       title="Change to dark mode"
     >
