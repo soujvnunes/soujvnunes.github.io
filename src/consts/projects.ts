36ab977a036bb6383f8510ea3b37838c320b7c08
Victor <soujvnunes@gmail.com>
2024-03-02 00:23:00 -0300

    feat(projects): translate

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -2,9 +2,8 @@ const PROJECTS = [
   {
     id: "1",
     href: "https://app.polkamarkets.com/",
-    title: "Polkamarkets App",
-    description:
-      "Design review and implementation of UI/UX to be integrated with MetaMask interface or other digital wallets services.",
+    title: "contributions_projects.0.title",
+    description: "contributions_projects.0.description",
     icon: {
       name: "polkamarkets",
     },
@@ -15,9 +14,8 @@ const PROJECTS = [
   {
     id: "2",
     href: "https://sites.ufal.br/ufal-60-anos/",
-    title: "UFAL 60 Anos",
-    description:
-      "Design and implementation of a virtual exhibition that shows the evolution of the Federal University of Alagoas.",
+    title: "contributions_projects.1.title",
+    description: "contributions_projects.1.description",
     icon: {
       name: "ufal60anos",
     },
@@ -28,9 +26,8 @@ const PROJECTS = [
   {
     id: "3",
     href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
-    title: "Ways to create Design Tokens",
-    description:
-      "Writing of step-by-step on how to create design tokens based on shipped web application and without documentation.",
+    title: "contributions_projects.2.title",
+    description: "contributions_projects.2.description",
     icon: {
       name: "medium",
     },
