1202899e04d13fa105eaae69ddc6ddb9c4a1e245
Victor <soujvnunes@gmail.com>
2024-03-02 00:23:10 -0300

    feat(ThemeButton): translate

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,14 +1,16 @@
+import { useTranslation } from "react-i18next";
 import classNames from "consts/classNames";
 import cn from "helpers/cn";
 import MaterialSymbols from "./MaterialSymbols";
 
 export default function ThemeButton() {
-  // todo: implement this URGENTLY
+  const [t] = useTranslation();
+
   return (
     <button
-      className={cn(classNames.button.root, "-mr-2")}
-      aria-label="Change to dark mode"
-      title="Change to dark mode"
+      className={cn(classNames.button.root, "mr-2")}
+      aria-label={t("change_mode.light")}
+      title={t("change_mode.light")}
     >
       <MaterialSymbols name="dark_mode" />
     </button>
