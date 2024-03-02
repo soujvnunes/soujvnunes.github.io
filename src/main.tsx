9ef11b52166510d515e2ba778381ebad141de68c
Victor <soujvnunes@gmail.com>
2024-03-02 00:22:43 -0300

    feat(main): implement i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./main.css";
+import "./i18n/config";
 
-const container = document.getElementById("root");
-const root = container ? createRoot(container) : null;
-
-root?.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
