493a41cc51825115c8dcd43c0ee5d695d96dd6dc
Victor <soujvnunes@gmail.com>
2024-03-03 20:32:45 -0300

    feat(LanguageButton): adjust label

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -4,12 +4,14 @@ import cn from "helpers/cn";
 
 export default function LanguageButton() {
   const [t, i18n] = useTranslation();
+  const title = t("change_lang");
 
   return (
     <button
+      aria-label={title}
+      title={title}
       name={i18n.language === "en" ? "pt" : "en"}
       className={cn(classNames.button.root, classNames.button.outline)}
-      aria-label={t(`change_lang.${i18n.language}`)}
       onClick={(event) => void i18n.changeLanguage(event.currentTarget.name)}
     >
       {i18n.language === "en" ? "en" : "pt"}
