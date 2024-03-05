dc283d2b9f8915edea0481f9cab7a45aa68881b7
Victor <soujvnunes@gmail.com>
2024-03-05 19:38:29 -0300

    feat(cn): remove unused modules

--- src/helpers/cn.test.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
-import { expect, test } from "vitest";
-import cn from "./cn";
-
-test("should return a string", () => {
-  expect(
-    cn("hello", true && "world", false && "foo", null, false, undefined),
-  ).toMatch(/hello world/);
-});
--- src/helpers/cn.ts
+++
@@ -1,3 +0,0 @@
-export default function cn(...params: (string | null | undefined | false)[]) {
-  return params.filter(Boolean).join(" ");
-}
