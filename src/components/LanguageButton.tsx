fa70ea041f1a04b4c754974c579be9f8c774628f
Victor <soujvnunes@gmail.com>
2024-03-05 19:37:54 -0300

    feat(LanguageButton): implement tw-merge

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
+import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import cn from "helpers/cn";
 
 export default function LanguageButton() {
   const [t, i18n] = useTranslation();
@@ -11,7 +11,7 @@ export default function LanguageButton() {
       aria-label={title}
       title={title}
       name={i18n.language === "en" ? "pt" : "en"}
-      className={cn(classNames.button.root, classNames.button.outline)}
+      className={twMerge(classNames.button.root, classNames.button.outline)}
       onClick={(event) => void i18n.changeLanguage(event.currentTarget.name)}
     >
       {i18n.language === "en" ? "en" : "pt"}
