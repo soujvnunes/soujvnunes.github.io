f762632e7b8f6bc9b2349fb4b7596c393531ac97
Victor <soujvnunes@gmail.com>
2024-03-06 12:31:46 -0300

    feat(eslint): add config path

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
           "/^react/",
           "module",
           "/consts/",
-          "/helpers/",
+          "/config/",
           "/hooks/",
           "/assets/",
           "/components/",
