c5b759bbff65532339832a2534078d9bf4bd2e1e
Victor <soujvnunes@gmail.com>
2024-03-11 11:47:00 -0300

    feat(Header): adjust SoujvnunesLogos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
 import LanguageButton from "./LanguageButton";
+import MaterialSymbols from "./MaterialSymbols";
 import SoujvnunesLogos from "./SoujvnunesLogos";
 
 export default function Header() {
@@ -12,7 +13,17 @@ export default function Header() {
         "flex h-16 items-center",
       )}
     >
-      <SoujvnunesLogos />
+      <a href="/" className="mr-auto" aria-label="Go to home">
+        <h1>
+          <SoujvnunesLogos />
+          <MaterialSymbols
+            name="&#xf755;"
+            className="-ml-1.5 -mr-1.5 align-middle"
+          />
+          design_
+          <strong>engineer</strong>
+        </h1>
+      </a>
       <LanguageButton />
     </header>
   );
