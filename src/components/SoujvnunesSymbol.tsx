05e0f419877901d02bf4abab52edd22d748238f2
Victor <soujvnunes@gmail.com>
2024-03-11 11:43:04 -0300

    feat(SoujvnunesSymbol): compose

---
+++ src/components/SoujvnunesSymbol.tsx
@@ -0,0 +1,11 @@
+export default function SoujvnunesSymbol() {
+  return (
+    <strong className="text-base">
+      <span className="tracking-[-0.2em]">
+        <span className="text-red-600 dark:text-red-400">j</span>
+        <span className="text-amber-600 dark:text-amber-400">v</span>
+      </span>
+      <span className="text-blue-600 dark:text-blue-400">n</span>
+    </strong>
+  );
+}
