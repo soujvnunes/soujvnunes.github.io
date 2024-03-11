f0f0f8956b3d68ba1a667bc328cddc2d8e01a458
Victor <soujvnunes@gmail.com>
2024-03-11 11:48:49 -0300

    feat(Footer): implement Socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,9 @@
-import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import Icon from "./Icon";
-
-const socials = [
-  {
-    href: "https://medium.com/@soujvnunes",
-    name: "Medium",
-  },
-  {
-    href: "https://github.com/soujvnunes",
-    name: "GitHub",
-  },
-  {
-    href: "https://www.linkedin.com/in/soujvnunes/",
-    name: "LinkedIn",
-  },
-
-  {
-    href: "https://www.behance.net/soujvnunes",
-    name: "Behance",
-  },
-] as const;
+import FooterSocials from "./FooterSocials";
+import SoujvnunesSymbol from "./SoujvnunesSymbol";
 
 export default function Footer() {
-  const [t] = useTranslation();
-
   return (
     <footer
       className={twMerge(
@@ -35,29 +13,9 @@ export default function Footer() {
       )}
     >
       <span className={classNames.text.secondary}>
-        Copyright (c) {new Date().getFullYear()} <strong>soujvnunes</strong>
+        <SoujvnunesSymbol /> (c) {new Date().getFullYear()}
       </span>
-      <ul className="ml-auto flex space-x-4">
-        {socials.map(({ name, href }) => {
-          const title = t("socials_label", {
-            name,
-          });
-
-          return (
-            <li key={name}>
-              <a
-                className="text-black/60 hover:text-black/80 dark:text-white/80 dark:hover:text-white"
-                target="_blank"
-                rel="noreferrer"
-                title={title}
-                href={href}
-              >
-                <Icon name={name} title={title} />
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <FooterSocials />
     </footer>
   );
 }
