5d6d9704e43f4f11a87a043b543abac416df4d83
Victor <soujvnunes@gmail.com>
2024-03-11 11:43:15 -0300

    feat(SoujvnunesLogos): implement symbol

--- src/components/SoujvnunesLogos.tsx
+++ src/components/SoujvnunesLogos.tsx
@@ -1,23 +1,11 @@
-import MaterialSymbols from "./MaterialSymbols";
+import SoujvnunesSymbol from "./SoujvnunesSymbol";
 
 export default function SoujvnunesLogos() {
   return (
-    <a href="/" className="mr-auto">
-      <h1>
-        <strong>
-          sou
-          <span className="text-red-600 dark:text-red-400">j</span>
-          <span className="text-amber-600 dark:text-amber-400">v</span>
-          <span className="text-blue-600 dark:text-blue-400">n</span>
-          unes
-        </strong>
-        <MaterialSymbols
-          name="pen_size_1"
-          className="-ml-1.5 -mr-1.5 align-middle"
-        />
-        design_
-        <strong>engineer</strong>
-      </h1>
-    </a>
+    <strong>
+      sou
+      <SoujvnunesSymbol />
+      unes
+    </strong>
   );
 }
