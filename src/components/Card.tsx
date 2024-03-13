0708073db7d42f1a83a5f8b84b3429ad6190b582
Victor <soujvnunes@gmail.com>
2024-03-13 20:11:40 -0300

    feat(Card): extract header and content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,43 +1,30 @@
-import { useId } from "react";
+import { useId, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
+import { CardContext } from "hooks/useCard";
 
-interface CardProps {
-  children: React.ReactNode;
-  startItem?: React.ReactNode;
-  endItem?: React.ReactNode;
-  label: string;
-  href: string;
-}
+type CardProps = Pick<React.ComponentPropsWithoutRef<"article">, "children">;
 
-export default function Card({
-  children,
-  label,
-  endItem,
-  startItem,
-}: CardProps) {
+export default function Card({ children }: CardProps) {
   const labelId = useId();
   const descriptionId = useId();
+  const [ids] = useState({
+    labelId,
+    descriptionId,
+  });
 
   return (
-    <article
-      aria-labelledby={labelId}
-      aria-describedby={descriptionId}
-      className={twMerge(classNames.surface.primary, "rounded-2xl p-2")}
-    >
-      <header className="flex items-center">
-        {startItem}
-        <h3 className="ml-2 truncate text-ellipsis font-bold" id={labelId}>
-          {label}
-        </h3>
-        {endItem}
-      </header>
-      <p
-        className={twMerge(classNames.text.secondary, "pb-2 pl-12")}
-        id={descriptionId}
+    <CardContext.Provider value={ids}>
+      <article
+        aria-labelledby={labelId}
+        aria-describedby={descriptionId}
+        className={twMerge(
+          classNames.surface.primary,
+          "flex h-full flex-col rounded-2xl p-2",
+        )}
       >
         {children}
-      </p>
-    </article>
+      </article>
+    </CardContext.Provider>
   );
 }
