fc7b8636f7ace7fc055e572800f8f82c31cc135f
Victor <soujvnunes@gmail.com>
2024-03-13 20:14:52 -0300

    feat(ContributionsSectionProjects): clarify project actions
    contribution, read, live, project

--- src/components/ContributionsSectionProjects.tsx
+++ src/components/ContributionsSectionProjects.tsx
@@ -1,45 +1,121 @@
+import { Fragment } from "react";
 import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import Card from "./Card";
-import Icon from "./Icon";
-import MaterialSymbols from "./MaterialSymbols";
+import Button from "components/Button";
+import Card from "components/Card";
+import CardContent from "components/CardContent";
+import CardFooter from "components/CardFooter";
+import CardHeader from "components/CardHeader";
+import Icon, { type IconProps } from "components/Icon";
 
-const projects = [
+interface Projects {
+  id: string;
+  actions: ((
+    | {
+        endItem: {
+          icon: "ArrowOutward";
+        };
+        id: "live";
+      }
+    | {
+        startItem: {
+          icon: "GitHub";
+        };
+        id: "contribution";
+      }
+    | {
+        startItem: {
+          icon: "Behance";
+        };
+        id: "project";
+      }
+    | {
+        startItem: {
+          icon: "Medium";
+        };
+        id: "article";
+      }
+  ) & {
+    href: string;
+  })[];
+  startItem: {
+    icon: IconProps["name"];
+    className: string;
+  };
+}
+
+const projects: Projects[] = [
   {
-    href: "https://app.polkamarkets.com/",
-    title: "contributions_projects.0.title",
-    description: "contributions_projects.0.description",
-    icon: {
-      name: "Polkamarkets",
-    },
-    className: {
-      startItem: "bg-blue-500/20",
+    id: "polkamarkets",
+    actions: [
+      {
+        id: "live",
+        href: "https://app.polkamarkets.com/",
+        endItem: {
+          icon: "ArrowOutward",
+        },
+      },
+      {
+        id: "contribution",
+        href: "https://github.com/Polkamarkets/polkamarkets-web/commits?author=soujvnunes",
+        startItem: {
+          icon: "GitHub",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-blue-500/10 to-blue-500/20",
+      icon: "Polkamarkets",
     },
   },
   {
-    href: "https://sites.ufal.br/ufal-60-anos/",
-    title: "contributions_projects.1.title",
-    description: "contributions_projects.1.description",
-    icon: {
-      name: "Ufal",
-    },
-    className: {
-      startItem: "bg-fuchsia-500/20",
+    id: "vitalk",
+    actions: [
+      {
+        id: "article",
+        href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
+        startItem: {
+          icon: "Medium",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-sky-500/10 to-sky-500/20",
+      icon: "Vitalk",
     },
   },
   {
-    href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
-    title: "contributions_projects.2.title",
-    description: "contributions_projects.2.description",
-    icon: {
-      name: "Medium",
-    },
-    className: {
-      startItem: "bg-gray-500/20",
+    id: "ufal",
+    actions: [
+      {
+        id: "live",
+        href: "https://sites.ufal.br/ufal-60-anos/",
+        endItem: {
+          icon: "ArrowOutward",
+        },
+      },
+      {
+        id: "contribution",
+        href: "https://github.com/soujvnunes/virtual-exhibition/commits?author=soujvnunes",
+        startItem: {
+          icon: "GitHub",
+        },
+      },
+      {
+        id: "project",
+        href: "https://www.behance.net/gallery/116828869/UFAL-60-Anos",
+        startItem: {
+          icon: "Behance",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-fuchsia-500/10 to-fuchsia-500/20",
+      icon: "Ufal",
     },
   },
-] as const;
+];
 
 export default function ContributionsSectionProjects() {
   const [t] = useTranslation();
@@ -47,38 +123,65 @@ export default function ContributionsSectionProjects() {
   return (
     <ul className={twMerge(classNames.grid.root, classNames.grid.col[4][0])}>
       {projects.map((project) => (
-        <li key={project.href} className={classNames.grid.col[4][1]}>
-          <Card
-            href={project.href}
-            label={t(project.title)}
-            startItem={
-              <span
-                className={twMerge(
-                  ...classNames.size.lg,
-                  "flex shrink-0 rounded-lg",
-                  project.className.startItem,
-                )}
-              >
-                <Icon className="m-auto" name={project.icon.name} />
-              </span>
-            }
-            endItem={
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={project.href}
-                aria-label={t("contributions_cta")}
-                className={twMerge(
-                  classNames.button.root,
-                  classNames.button.text,
-                  "ml-auto",
-                )}
-              >
-                <MaterialSymbols name="&#xf8ce;" />
-              </a>
-            }
-          >
-            {t(project.description)}
+        <li key={project.id} className={classNames.grid.col[4][1]}>
+          <Card>
+            <CardHeader
+              subhead={t(`contributions_projects.${project.id}.company`)}
+              startItem={
+                <span
+                  className={twMerge(
+                    ...classNames.size.lg,
+                    "flex shrink-0 rounded-lg",
+                    project.startItem.className,
+                  )}
+                >
+                  <Icon className="m-auto" name={project.startItem.icon} />
+                </span>
+              }
+            >
+              {t(`contributions_projects.${project.id}.title`)}
+            </CardHeader>
+            <CardContent>
+              {t(`contributions_projects.${project.id}.description`)}
+            </CardContent>
+            <CardFooter>
+              {project.actions.map((action) => {
+                const actionValue = t(
+                  `contributions_projects_actions.${action.id}`,
+                );
+                const label = t("contributions_projects_actions_label", {
+                  action: actionValue,
+                });
+
+                return (
+                  <Fragment key={action.id}>
+                    <Button
+                      as="a"
+                      size="sm"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label={label}
+                      title={label}
+                      href={action.href}
+                      className={action.id === "live" ? "mr-auto" : ""}
+                      variant={action.id === "live" ? "outline" : "text"}
+                      endItem={
+                        "endItem" in action && (
+                          <Icon name={action.endItem.icon} />
+                        )
+                      }
+                      startItem={
+                        "startItem" in action && (
+                          <Icon name={action.startItem.icon} />
+                        )
+                      }
+                    >
+                      {actionValue}
+                    </Button>
+                  </Fragment>
+                );
+              })}
+            </CardFooter>
           </Card>
         </li>
       ))}
