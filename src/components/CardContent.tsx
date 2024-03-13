d3adf3d0a5877bf612e61e4c2bfe027bc1f2b176
Victor <soujvnunes@gmail.com>
2024-03-13 20:11:54 -0300

    feat(CardContent): compose

---
+++ src/components/CardContent.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+import classNames from "consts/classNames";
+import useCard from "hooks/useCard";
+
+type CardContentProps = Pick<React.ComponentPropsWithoutRef<"p">, "children">;
+
+export default function CardContent({ children }: CardContentProps) {
+  const card = useCard();
+
+  return (
+    <p
+      className={twMerge(classNames.text.secondary, "pb-2 pl-2 pr-2 pt-2")}
+      id={card.descriptionId}
+    >
+      {children}
+    </p>
+  );
+}
