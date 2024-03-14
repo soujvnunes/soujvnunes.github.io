12185b394c667c1ae3775cc45919dc24ba31e050
Victor <soujvnunes@gmail.com>
2024-03-14 08:41:42 -0300

    feat(main): implement root

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode, Suspense } from "react";
-import { createRoot } from "react-dom/client";
 import "config/i18n";
-import "./main.css";
 import SoujvnunesLogos from "components/SoujvnunesLogos";
 import LazyApp from "./LazyApp";
+import root from "./root";
+import "./main.css";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById("root")!).render(
+root?.render(
   <StrictMode>
     <Suspense
       fallback={<SoujvnunesLogos className="m-auto animate-pulse text-2xl" />}
