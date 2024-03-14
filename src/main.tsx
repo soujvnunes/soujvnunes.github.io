78c90df17461b3e427a3e9a0f874b543e9d7ea9d
Victor <soujvnunes@gmail.com>
2024-03-14 09:54:36 -0300

    feat(main): move config modules to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode, Suspense } from "react";
-import "config/i18n";
-import SoujvnunesLogos from "components/SoujvnunesLogos";
+import SoujvnunesLogos from "containers/SoujvnunesLogos";
 import LazyApp from "./LazyApp";
 import root from "./root";
-import "./main.css";
 
 root?.render(
   <StrictMode>
