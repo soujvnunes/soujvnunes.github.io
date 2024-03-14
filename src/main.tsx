6d5fc74729afd01354c6fdec267c85b7f01af8bb
Victor <soujvnunes@gmail.com>
2024-03-13 21:07:46 -0300

    feat(main): implement lazy app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "config/i18n";
-import App from "./App";
 import "./main.css";
+import SoujvnunesLogos from "components/SoujvnunesLogos";
+import LazyApp from "./LazyApp";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Suspense
+      fallback={<SoujvnunesLogos className="m-auto animate-pulse text-2xl" />}
+    >
+      <LazyApp />
+    </Suspense>
   </StrictMode>,
 );
