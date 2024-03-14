5ac55bf4fcfa530276e1901951c37552b7f5da01
Victor <soujvnunes@gmail.com>
2024-03-13 21:06:48 -0300

    feat(SoujvnunesSymbol): extends props

--- src/components/SoujvnunesSymbol.tsx
+++ src/components/SoujvnunesSymbol.tsx
@@ -1,6 +1,11 @@
-export default function SoujvnunesSymbol() {
+type SoujvnunesSymbolProps = Pick<
+  React.ComponentPropsWithoutRef<"strong">,
+  "className"
+>;
+
+export default function SoujvnunesSymbol(props: SoujvnunesSymbolProps) {
   return (
-    <strong className="text-base">
+    <strong {...props}>
       <span className="tracking-[-0.2em]">
         <span className="text-red-600 dark:text-red-400">j</span>
         <span className="text-amber-600 dark:text-amber-400">v</span>
