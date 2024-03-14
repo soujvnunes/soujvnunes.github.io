1f99bdf18ea6c7de7b4c005b629f3777879bd9a7
Victor <soujvnunes@gmail.com>
2024-03-14 10:38:36 -0300

    fix(App): move config modules to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "config/i18n";
 import Contributions from "containers/Contributions";
 import Footer from "containers/Footer";
 import Header from "containers/Header";
 import Intro from "containers/Intro";
-import "./main.css";
 
 export default function App() {
   return (
