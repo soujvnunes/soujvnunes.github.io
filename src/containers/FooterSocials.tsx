87c26ae35b0d3eee5ff13aff4377eb4aaea94839
Victor <soujvnunes@gmail.com>
2024-03-14 09:48:05 -0300

    feat(FooterSocials): move to containers

--- src/containers/FooterSocials.tsx
+++ src/containers/FooterSocials.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
+import Button from "components/Button";
 import Icon from "components/Icon";
-import Button from "./Button";
 
 const socials = [
   {
