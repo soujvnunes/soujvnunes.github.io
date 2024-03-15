86945245f8d228f27625bebfb6a369f06901b245
Victor <soujvnunes@gmail.com>
2024-03-15 19:13:30 -0300

    feat(Footer): adjust container spacing

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -6,17 +6,13 @@ import SoujvnunesSymbol from "./SoujvnunesSymbol";
 export default function Footer() {
   return (
     <footer
-      className={twMerge(
-        classNames.surface.primary,
-        classNames.container.root,
-        "flex h-10 items-center",
-      )}
+      className={twMerge(classNames.container.root, "flex h-16 items-center")}
     >
       <p className={twMerge(classNames.text.secondary, "leading-[0]")}>
         <SoujvnunesSymbol />
         <span className="text-xs"> &copy; {new Date().getFullYear()}</span>
       </p>
-      <ul className="ml-auto space-x-1">
+      <ul className="ml-auto space-x-2">
         <FooterSocials />
       </ul>
     </footer>
