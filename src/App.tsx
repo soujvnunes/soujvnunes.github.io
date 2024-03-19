4f0cd3d6f71aefdc4afe3b9e5fdd52e9d88306e3
Victor <soujvnunes@gmail.com>
2024-03-19 17:22:39 -0300

    feat(App) add tab panels structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { Tab } from "@headlessui/react";
+import classNames from "consts/classNames";
 import Contributions from "containers/Contributions";
 import Footer from "containers/Footer";
 import Header from "containers/Header";
-import Intro from "containers/Intro";
 
 export default function App() {
   return (
-    <>
+    <Tab.Group as="div" className={classNames.container.root}>
       <Header />
-      <main className="flex grow flex-col">
-        <Intro />
-        <Contributions />
-        <Footer />
-      </main>
-    </>
+      <Tab.Panels as="main">
+        <Tab.Panel>
+          <Contributions />
+        </Tab.Panel>
+        <Tab.Panel>About</Tab.Panel>
+      </Tab.Panels>
+      <Footer />
+    </Tab.Group>
   );
 }
