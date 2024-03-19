17948d0deb6c768f1bcd67e8b4d1f1aed3f8e62e
Victor <soujvnunes@gmail.com>
2024-03-19 17:24:15 -0300

    feat(Contributions) move title as tab to header

--- src/containers/Contributions.tsx
+++ src/containers/Contributions.tsx
@@ -1,33 +1,17 @@
-import { useId } from "react";
 import { useTranslation } from "react-i18next";
-import { twMerge } from "tailwind-merge";
-import classNames from "consts/classNames";
 import ContributionsProjects from "./ContributionsProjects";
 
 export default function Contributions() {
   const [t] = useTranslation();
-  const labelId = useId();
 
   return (
-    <section className={classNames.container.root}>
-      <h2
-        id={labelId}
-        className={twMerge(
-          classNames.text.color.secondary,
-          classNames.text.variant.title,
-          "mb-2 lg:mb-4",
-        )}
-      >
-        {t("contributions_title")}
-      </h2>
-      <ul
-        role="feed"
-        aria-busy="false"
-        aria-labelledby={labelId}
-        className={twMerge(classNames.grid.root, classNames.grid.col[4][0])}
-      >
-        <ContributionsProjects />
-      </ul>
-    </section>
+    <ul
+      role="feed"
+      aria-busy="false"
+      aria-label={t("contributions_title")}
+      className="space-y-4"
+    >
+      <ContributionsProjects />
+    </ul>
   );
 }
