41ed76e546f43360160c244d24d01079ba327518
Victor <soujvnunes@gmail.com>
2024-03-21 18:38:51 -0300

    feat(App) adjust default tab based on search params

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { Tab } from "@headlessui/react";
 import classNames from "consts/classNames";
+import useTab from "hooks/useTab";
 import Contributions from "containers/Contributions";
 import Footer from "containers/Footer";
 import Header from "containers/Header";
+import HeaderIntro from "containers/HeaderIntro";
+import HeaderNav from "containers/HeaderNav";
 
 export default function App() {
+  const tab = useTab();
+
   return (
-    <Tab.Group as="div" className={classNames.container.root}>
-      <Header />
-      <Tab.Panels as="main">
-        <Tab.Panel>
-          <Contributions />
-        </Tab.Panel>
+    <Tab.Group defaultIndex={tab.current === tab.group.contributions ? 0 : 1}>
+      <Header className={classNames.container.root}>
+        <HeaderIntro />
+        <HeaderNav />
+      </Header>
+      <Tab.Panels as="main" className={classNames.container.root}>
+        <Contributions />
         <Tab.Panel>About</Tab.Panel>
       </Tab.Panels>
       <Footer />
