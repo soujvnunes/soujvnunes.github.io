77229f3a2b81c2b85128550eadff540665e4f057
Victor <soujvnunes@gmail.com>
2024-03-21 18:34:25 -0300

    feat(HeaderIntro) adjust UI
    - memoji container
    - title 1ry, 2ry colors

--- src/containers/HeaderIntro.tsx
+++ src/containers/HeaderIntro.tsx
@@ -1,27 +1,38 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import HeaderMemoji from "./HeaderMemoji";
+import HeaderIntroMemoji from "./HeaderIntroMemoji";
 
 export default function HeaderIntro() {
   const [t] = useTranslation();
 
   return (
     <>
-      <p
-        className={twMerge(
-          classNames.text.color.secondary,
-          "inline-flex h-16 items-center rounded-full bg-gradient-to-r from-indigo-500/40 to-amber-500/40 pr-8",
-        )}
-      >
-        <HeaderMemoji />
-        <strong>
+      <p className="inline-flex items-center">
+        <HeaderIntroMemoji />
+        <strong
+          className={twMerge(
+            classNames.text.color.secondary,
+            "-ml-10 rounded-full bg-gradient-to-r from-amber-500/20 to-indigo-500/20 px-5 py-3 lg:-ml-14 lg:px-8 lg:py-5",
+          )}
+        >
           {t("intro_caption_title")}{" "}
-          <span className="text-black/80 dark:text-white">Victor</span>
+          <span className={classNames.text.color.primary}>Victor</span>
         </strong>
       </p>
-      <h1 className={twMerge(classNames.text.variant.headline, "-mt-6 mb-10")}>
-        {t("intro_title")}
+      <h1
+        className={twMerge(
+          classNames.text.variant.headline,
+          "-mt-4 mb-10 lg:-mt-8",
+        )}
+      >
+        <Trans
+          i18nKey="intro_title"
+          components={{
+            primary: <span className="text-indigo-500" />,
+            secondary: <span className="text-amber-500" />,
+          }}
+        />
       </h1>
     </>
   );
