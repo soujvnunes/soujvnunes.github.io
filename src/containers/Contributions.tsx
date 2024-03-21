af7f508f47566c957e806ae17f77d7482e96ba30
Victor <soujvnunes@gmail.com>
2024-03-21 18:29:47 -0300

    feat(Contributions) compose projects

--- src/containers/Contributions.tsx
+++ src/containers/Contributions.tsx
@@ -1,17 +1,107 @@
 import { useTranslation } from "react-i18next";
-import ContributionsProjects from "./ContributionsProjects";
+import { Tab } from "@headlessui/react";
+import Project, { type ProjectProps } from "./Project";
 
 export default function Contributions() {
   const [t] = useTranslation();
 
   return (
-    <ul
-      role="feed"
-      aria-busy="false"
-      aria-label={t("contributions_title")}
-      className="space-y-4"
-    >
-      <ContributionsProjects />
-    </ul>
+    <Tab.Panel>
+      <ul
+        role="feed"
+        aria-busy="false"
+        aria-label={t("contributions_title")}
+        className="space-y-2 lg:space-y-4"
+      >
+        {projects.map((project) => (
+          <li key={project.id}>
+            <Project {...project} />
+          </li>
+        ))}
+      </ul>
+    </Tab.Panel>
   );
 }
+
+const projects: ProjectProps[] = [
+  {
+    id: "polkamarkets",
+    actions: [
+      {
+        id: "contribution",
+        href: "https://github.com/Polkamarkets/polkamarkets-web/commits?author=soujvnunes",
+        startItem: {
+          icon: "GitHub",
+        },
+      },
+    ],
+    endItem: {
+      href: "https://app.polkamarkets.com/",
+    },
+    startItem: {
+      className: "bg-gradient-to-br from-blue-500/10 to-blue-500/20",
+      icon: "Polkamarkets",
+    },
+  },
+  {
+    id: "vitalk",
+    actions: [
+      {
+        id: "article",
+        href: "https://medium.com/@soujvnunes/uma-das-possíveis-maneiras-de-criar-design-tokens-d52a8c315b2",
+        startItem: {
+          icon: "Medium",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-sky-500/10 to-sky-500/20",
+      icon: "Vitalk",
+    },
+  },
+  {
+    id: "bepro",
+    actions: [
+      {
+        id: "contribution",
+        href: "https://github.com/bepronetwork/web-network/commits?author=soujvnunes",
+        startItem: {
+          icon: "GitHub",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-indigo-500/10 to-indigo-500/20",
+      icon: "BeproNetwork",
+    },
+    endItem: {
+      href: "https://app.bepro.network",
+    },
+  },
+  {
+    id: "ufal",
+    actions: [
+      {
+        id: "contribution",
+        href: "https://github.com/soujvnunes/virtual-exhibition/commits?author=soujvnunes",
+        startItem: {
+          icon: "GitHub",
+        },
+      },
+      {
+        id: "project",
+        href: "https://www.behance.net/gallery/116828869/UFAL-60-Anos",
+        startItem: {
+          icon: "Behance",
+        },
+      },
+    ],
+    startItem: {
+      className: "bg-gradient-to-br from-fuchsia-500/10 to-fuchsia-500/20",
+      icon: "Ufal",
+    },
+    endItem: {
+      href: "https://sites.ufal.br/ufal-60-anos/",
+    },
+  },
+];
