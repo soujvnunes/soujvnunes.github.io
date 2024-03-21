b17b66647f5d9b72692781b6fcc9ff19858cdc0b
Victor <soujvnunes@gmail.com>
2024-03-21 18:35:06 -0300

    feat(Header) prefer using as a container

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import HeaderIntro from "./HeaderIntro";
-import HeaderNav from "./HeaderNav";
 
-export default function Header() {
-  const [t] = useTranslation();
+type HeaderProps = Pick<
+  React.ComponentPropsWithoutRef<"header">,
+  "children" | "className"
+>;
+
+export default function Header({ className, children }: HeaderProps) {
   const [height, setHeight] = useState(0);
   const handleHeader = useCallback((node: HTMLElement | null) => {
     if (node !== null) {
@@ -15,23 +16,24 @@ export default function Header() {
   }, []);
 
   return (
-    <>
+    <header
+      className={twMerge("pb-2 pt-10 text-center lg:pb-4", className)}
+      ref={handleHeader}
+    >
       <div
-        aria-hidden
+        style={{
+          height: height + 56,
+        }}
         className={twMerge(
           classNames.surface.primary,
           "absolute inset-x-0 top-0 -z-10",
         )}
-        style={{ height: height + 64 }}
       >
-        <div className="h-full w-full bg-gradient-to-r from-indigo-500/40 to-amber-500/40">
-          <div className="h-full w-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-100 dark:from-stone-900" />
+        <div className="h-full w-full bg-gradient-to-r from-amber-500/40 to-indigo-500/40 dark:from-amber-500/20 dark:to-indigo-500/20">
+          <div className="h-full w-full bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-gray-100 lg:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-stone-900" />
         </div>
       </div>
-      <header ref={handleHeader} className="pb-4 pt-10 text-center">
-        <HeaderIntro />
-        <HeaderNav />
-      </header>
-    </>
+      {children}
+    </header>
   );
 }
