0b72b15c944755a766ff5ca03da25c0223335711
Victor <soujvnunes@gmail.com>
2024-03-21 18:38:14 -0300

    feat(HeaderNav) implement tabs

--- src/containers/HeaderNav.tsx
+++ src/containers/HeaderNav.tsx
@@ -2,32 +2,35 @@ import { useTranslation } from "react-i18next";
 import { Tab } from "@headlessui/react";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
+import objectKeys from "helpers/objectKeys";
+import useTab from "hooks/useTab";
 import Button from "components/Button";
 import Icon from "components/Icon";
 
 export default function HeaderNav() {
+  const tab = useTab();
   const [t, i18n] = useTranslation();
   const title = t("change_lang");
 
   return (
     <nav className="mx-2 flex items-center">
       <Tab.List className="mr-auto space-x-2 lg:space-x-4">
-        <Tab
-          className={twMerge(
-            classNames.text.color.secondary,
-            classNames.text.variant.title,
-          )}
-        >
-          {t("contributions_title")}
-        </Tab>
-        <Tab
-          className={twMerge(
-            classNames.text.color.secondary,
-            classNames.text.variant.title,
-          )}
-        >
-          {t("about_title")}
-        </Tab>
+        {objectKeys(tab.group).map((_tab) => (
+          <Tab
+            key={_tab}
+            value={_tab}
+            onClick={tab.set}
+            className={({ selected }) =>
+              twMerge(
+                classNames.text.color.secondary,
+                classNames.text.variant.title,
+                selected && classNames.text.color.primary,
+              )
+            }
+          >
+            {t(`tabs.${_tab}`)}
+          </Tab>
+        ))}
       </Tab.List>
       <Button
         className="plausible-event-name=Toggled+language min-w-32"
