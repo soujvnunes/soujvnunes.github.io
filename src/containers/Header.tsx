b933aa58721181acdb7d4558ecd0dc0dc9cd2a1c
Victor <soujvnunes@gmail.com>
2024-03-22 12:08:53 -0300

    feat:Header:adjust backdrop colors

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -2,12 +2,9 @@ import { useCallback, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
 
-type HeaderProps = Pick<
-  React.ComponentPropsWithoutRef<"header">,
-  "children" | "className"
->;
+type HeaderProps = Pick<React.ComponentPropsWithoutRef<"header">, "children">;
 
-export default function Header({ className, children }: HeaderProps) {
+export default function Header({ children }: HeaderProps) {
   const [height, setHeight] = useState(0);
   const handleHeader = useCallback((node: HTMLElement | null) => {
     if (node !== null) {
@@ -17,8 +14,11 @@ export default function Header({ className, children }: HeaderProps) {
 
   return (
     <header
-      className={twMerge("pb-2 pt-10 text-center lg:pb-4", className)}
       ref={handleHeader}
+      className={twMerge(
+        classNames.container.root,
+        "pb-2 pt-10 text-center lg:pb-4",
+      )}
     >
       <div
         style={{
@@ -29,7 +29,7 @@ export default function Header({ className, children }: HeaderProps) {
           "absolute inset-x-0 top-0 -z-10",
         )}
       >
-        <div className="h-full w-full bg-gradient-to-r from-amber-500/40 to-indigo-500/40 dark:from-amber-500/20 dark:to-indigo-500/20">
+        <div className="h-full w-full bg-gradient-to-r from-indigo-500/40 to-amber-500/40 dark:from-indigo-500/20 dark:to-amber-500/20">
           <div className="h-full w-full bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-gray-100 lg:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-stone-900" />
         </div>
       </div>
