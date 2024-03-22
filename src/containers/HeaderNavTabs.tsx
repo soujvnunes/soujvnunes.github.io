5e046c311f621cf6ec39149ccf1461fe73d9c2cd
Victor <soujvnunes@gmail.com>
2024-03-22 10:27:45 -0300

    feat(HeaderNavTabs): compose

---
+++ src/containers/HeaderNavTabs.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+import { Tab } from "@headlessui/react";
+import { twMerge } from "tailwind-merge";
+import classNames from "consts/classNames";
+import objectKeys from "helpers/objectKeys";
+import useTab from "hooks/useTab";
+import Button from "components/Button";
+
+export default function HeaderNavTabs() {
+  const tab = useTab();
+  const [t] = useTranslation();
+
+  return (
+    <Tab.List className="mr-auto space-x-2 lg:space-x-4">
+      {objectKeys(tab.group).map((_tab) => (
+        <Tab
+          variant="text"
+          key={_tab}
+          value={_tab}
+          onClick={tab.set}
+          as={
+            window.matchMedia("(min-width: 1024px)").matches
+              ? undefined
+              : Button
+          }
+          className={twMerge(
+            classNames.text.color.secondary,
+            classNames.text.variant.title,
+            "inline-flex ui-selected:text-black/80 ui-selected:dark:text-white",
+          )}
+        >
+          {t(`tabs.${_tab}`)}
+        </Tab>
+      ))}
+    </Tab.List>
+  );
+}
