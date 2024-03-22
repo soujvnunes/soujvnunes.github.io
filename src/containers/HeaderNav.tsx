0bb22e66b87ceb7b864408c7ad57d3cea12f2bbf
Victor <soujvnunes@gmail.com>
2024-03-22 11:43:58 -0300

    feat:HeaderNav:adjust mobile UI

--- src/containers/HeaderNav.tsx
+++ src/containers/HeaderNav.tsx
@@ -3,7 +3,7 @@ import HeaderNavTabs from "./HeaderNavTabs";
 
 export default function HeaderNav() {
   return (
-    <nav className="flex items-center px-2 max-lg:fixed max-lg:inset-x-0 max-lg:bottom-0 max-lg:m-2 max-lg:h-[var(--header-nav)] max-lg:rounded-2xl max-lg:border-t max-lg:border-t-[#1E1F48] max-lg:bg-[#0A0A18]/60 max-lg:backdrop-blur-3xl lg:px-2">
+    <nav className="flex items-center px-2">
       <HeaderNavTabs />
       <HeaderNavLanguage />
     </nav>
