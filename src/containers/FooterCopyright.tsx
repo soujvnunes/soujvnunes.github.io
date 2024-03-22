ef35c24621e314523040fa0694c47483b543a0c4
Victor <soujvnunes@gmail.com>
2024-03-22 12:07:44 -0300

    feat:FooterCopyright: compose

---
+++ src/containers/FooterCopyright.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+import classNames from "consts/classNames";
+import SoujvnunesSymbol from "./SoujvnunesSymbol";
+
+export default function FooterCopyright() {
+  return (
+    <p className={twMerge(classNames.text.color.secondary, "leading-[0]")}>
+      <SoujvnunesSymbol />
+      <span className="text-xs"> &copy; {fullYear}</span>
+    </p>
+  );
+}
+
+const fullYear = new Date().getFullYear();
