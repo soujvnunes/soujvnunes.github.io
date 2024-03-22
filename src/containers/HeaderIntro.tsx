d5b82903810e4d7402ef09db3a009d7b7b305234
Victor <soujvnunes@gmail.com>
2024-03-22 11:44:42 -0300

    feat:HeaderIntro:adjust heading colors

--- src/containers/HeaderIntro.tsx
+++ src/containers/HeaderIntro.tsx
@@ -8,29 +8,24 @@ export default function HeaderIntro() {
 
   return (
     <>
-      <p className="inline-flex items-center">
-        <HeaderIntroMemoji />
+      <p>
+        <HeaderIntroMemoji className="-mr-10 align-middle lg:-mr-14" />
         <strong
           className={twMerge(
             classNames.text.color.secondary,
-            "-ml-10 rounded-full bg-gradient-to-r from-amber-500/20 to-indigo-500/20 px-5 py-3 lg:-ml-14 lg:px-8 lg:py-5",
+            "rounded-3xl border-b border-b-amber-700/10 bg-gradient-to-tl from-amber-500/20 px-5 py-3 max-lg:text-xs lg:rounded-full lg:px-8 lg:py-5 dark:border-b-0 dark:border-t dark:border-t-amber-500/10",
           )}
         >
           {t("intro_caption_title")}{" "}
           <span className={classNames.text.color.primary}>Victor</span>
         </strong>
       </p>
-      <h1
-        className={twMerge(
-          classNames.text.variant.headline,
-          "-mt-4 mb-10 lg:-mt-8",
-        )}
-      >
+      <h1 className={twMerge(classNames.text.variant.headline, "mb-10")}>
         <Trans
           i18nKey="intro_title"
           components={{
-            primary: <span className="text-indigo-500" />,
-            secondary: <span className="text-amber-500" />,
+            primary: <span className="text-amber-500" />,
+            secondary: <span className="text-indigo-500" />,
           }}
         />
       </h1>
