c440204e9b833ff8de221780f76d0d63f62f5695
Victor <soujvnunes@gmail.com>
2024-03-22 10:28:01 -0300

    feat(HeaderNav) implement tabs and language button

--- src/containers/HeaderNav.tsx
+++ src/containers/HeaderNav.tsx
@@ -1,49 +1,11 @@
-import { useTranslation } from "react-i18next";
-import { Tab } from "@headlessui/react";
-import { twMerge } from "tailwind-merge";
-import classNames from "consts/classNames";
-import objectKeys from "helpers/objectKeys";
-import useTab from "hooks/useTab";
-import Button from "components/Button";
-import Icon from "components/Icon";
+import HeaderNavLanguage from "./HeaderNavLanguage";
+import HeaderNavTabs from "./HeaderNavTabs";
 
 export default function HeaderNav() {
-  const tab = useTab();
-  const [t, i18n] = useTranslation();
-  const title = t("change_lang");
-
   return (
-    <nav className="mx-2 flex items-center">
-      <Tab.List className="mr-auto space-x-2 lg:space-x-4">
-        {objectKeys(tab.group).map((_tab) => (
-          <Tab
-            key={_tab}
-            value={_tab}
-            onClick={tab.set}
-            className={({ selected }) =>
-              twMerge(
-                classNames.text.color.secondary,
-                classNames.text.variant.title,
-                selected && classNames.text.color.primary,
-              )
-            }
-          >
-            {t(`tabs.${_tab}`)}
-          </Tab>
-        ))}
-      </Tab.List>
-      <Button
-        className="plausible-event-name=Toggled+language min-w-32"
-        aria-label={title}
-        title={title}
-        endItem={<Icon name="Language" />}
-        value={i18n.language === "en" ? "pt" : "en"}
-        onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-          void i18n.changeLanguage(event.currentTarget.value)
-        }
-      >
-        {t("lang")}
-      </Button>
+    <nav className="flex items-center px-2 max-lg:fixed max-lg:inset-x-0 max-lg:bottom-0 max-lg:m-2 max-lg:h-[var(--header-nav)] max-lg:rounded-2xl max-lg:border-t max-lg:border-t-[#1E1F48] max-lg:bg-[#0A0A18]/60 max-lg:backdrop-blur-3xl lg:px-2">
+      <HeaderNavTabs />
+      <HeaderNavLanguage />
     </nav>
   );
 }
