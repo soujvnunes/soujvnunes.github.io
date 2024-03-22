c088c93ddaf782cf4f4956da16d5182146da9eb7
Victor <soujvnunes@gmail.com>
2024-03-22 12:09:11 -0300

    feat:App:implement main

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { Tab } from "@headlessui/react";
-import classNames from "consts/classNames";
 import useTab from "hooks/useTab";
 import Contributions from "containers/Contributions";
 import Footer from "containers/Footer";
 import Header from "containers/Header";
 import HeaderIntro from "containers/HeaderIntro";
 import HeaderNav from "containers/HeaderNav";
+import Main from "containers/Main";
 
 export default function App() {
   const tab = useTab();
 
   return (
     <Tab.Group defaultIndex={tab.current === tab.group.contributions ? 0 : 1}>
-      <Header className={classNames.container.root}>
+      <Header>
         <HeaderIntro />
         <HeaderNav />
       </Header>
-      <Tab.Panels as="main" className={classNames.container.root}>
+      <Main>
         <Contributions />
         <Tab.Panel>About</Tab.Panel>
-      </Tab.Panels>
+      </Main>
       <Footer />
     </Tab.Group>
   );
