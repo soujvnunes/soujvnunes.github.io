18ace9c808d81e6c22291b1953c07a0e2446e47e
Victor <soujvnunes@gmail.com>
2024-03-22 11:43:28 -0300

    feat:HeaderNavTabs:revert isLgScreen

--- src/containers/HeaderNavTabs.tsx
+++ src/containers/HeaderNavTabs.tsx
@@ -2,10 +2,8 @@ import { useTranslation } from "react-i18next";
 import { Tab } from "@headlessui/react";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import isLgScreen from "helpers/isLgScreen";
 import objectKeys from "helpers/objectKeys";
 import useTab from "hooks/useTab";
-import Button from "components/Button";
 
 export default function HeaderNavTabs() {
   const tab = useTab();
@@ -15,11 +13,9 @@ export default function HeaderNavTabs() {
     <Tab.List className="mr-auto space-x-2 lg:space-x-4">
       {objectKeys(tab.group).map((_tab) => (
         <Tab
-          variant="text"
           key={_tab}
           value={_tab}
           onClick={tab.set}
-          as={isLgScreen ? undefined : Button}
           className={twMerge(
             classNames.text.color.secondary,
             classNames.text.variant.title,
