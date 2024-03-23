46dad710e9accdd0701104497f0e02d03a89a448
Victor <soujvnunes@gmail.com>
2024-03-23 12:43:05 -0300

    feat:main:implement suspense fallback with new UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode, Suspense } from "react";
 import "config/i18n";
-import SoujvnunesLogos from "containers/SoujvnunesLogos";
+import SuspenseFallback from "containers/SuspenseFallback";
 import LazyApp from "./LazyApp";
 import root from "./root";
 import "./main.css";
 
 root?.render(
   <StrictMode>
-    <Suspense
-      fallback={
-        <div className="flex h-dvh">
-          <SoujvnunesLogos className="m-auto animate-pulse text-2xl" />
-        </div>
-      }
-    >
+    <Suspense fallback={<SuspenseFallback />}>
       <LazyApp />
     </Suspense>
   </StrictMode>,
