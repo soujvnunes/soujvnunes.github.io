571af51b2a566425bf00f46286159aff36d27b3a
Victor <soujvnunes@gmail.com>
2024-03-23 12:34:03 -0300

    feat:Card:remove default surface UI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,5 @@
 import { useId, useState } from "react";
 import { twMerge } from "tailwind-merge";
-import classNames from "consts/classNames";
 import { CardContext } from "hooks/useCard";
 
 type CardProps = Pick<
@@ -21,11 +20,7 @@ export default function Card({ className, ...props }: CardProps) {
       <article
         aria-labelledby={labelId}
         aria-describedby={descriptionId}
-        className={twMerge(
-          classNames.surface.primary,
-          "flex h-full flex-col rounded-2xl p-2",
-          className,
-        )}
+        className={twMerge("flex h-full flex-col rounded-2xl p-2", className)}
         {...props}
       />
     </CardContext.Provider>
