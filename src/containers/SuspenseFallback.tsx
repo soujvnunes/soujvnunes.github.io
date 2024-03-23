bc22f972eaaedca50c92d7b98e52a520f66b4871
Victor <soujvnunes@gmail.com>
2024-03-23 12:42:48 -0300

    feat:SuspenseFallback:compose

---
+++ src/containers/SuspenseFallback.tsx
@@ -0,0 +1,16 @@
+import Socials from "./Socials";
+import SoujvnunesLogos from "./SoujvnunesLogos";
+
+export default function SuspenseFallback() {
+  return (
+    <div className="flex h-dvh">
+      <span className="m-auto text-center">
+        <h1>
+          <SoujvnunesLogos className="m-auto animate-pulse text-2xl" />
+        </h1>
+        <p className="mb-4">victor nunes - design engineer</p>
+        <Socials />
+      </span>
+    </div>
+  );
+}
