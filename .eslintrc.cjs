0c58c27f4544d39fb2a3014fe7c3b40cfc7b3057
Victor <soujvnunes@gmail.com>
2024-03-26 12:45:41 -0300

    feat:eslint:list providers alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,6 +38,7 @@ module.exports = {
           "/^consts/",
           "/^config/",
           "/^helpers/",
+          "/^providers/",
           "/^hooks/",
           "/^components/",
           "/^containers/",
