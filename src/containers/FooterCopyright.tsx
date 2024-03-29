989472eca5154dc38197e4ff06ec63659a129ef6
Victor <soujvnunes@gmail.com>
2024-03-28 21:33:58 -0300

    feat:FooterCopyright:change symbol to logo

--- src/containers/FooterCopyright.tsx
+++ src/containers/FooterCopyright.tsx
@@ -1,11 +1,11 @@
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import SoujvnunesSymbol from "./SoujvnunesSymbol";
+import SoujvnunesLogos from "./SoujvnunesLogos";
 
 export default function FooterCopyright() {
   return (
-    <p className={twMerge(classNames.text.color.secondary, "pl-4 leading-[0]")}>
-      <SoujvnunesSymbol />
+    <p className={twMerge(classNames.text.color.secondary, "py-3 leading-[0]")}>
+      <SoujvnunesLogos />
       <span className="text-xs"> &copy; {fullYear}</span>
     </p>
   );
