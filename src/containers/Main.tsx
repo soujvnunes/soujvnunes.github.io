703212f40fbbb73fcd1639b180c98a0042958d91
Victor <soujvnunes@gmail.com>
2024-03-29 13:58:22 -0300

    feat:Main:remove tab group

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,10 +1,7 @@
-import { Tab } from "@headlessui/react";
 import classNames from "consts/classNames";
 
 type MainProps = Pick<React.ComponentPropsWithoutRef<"main">, "children">;
 
 export default function Main(props: MainProps) {
-  return (
-    <Tab.Panels as="main" className={classNames.container.root} {...props} />
-  );
+  return <main className={classNames.container.root} {...props} />;
 }
