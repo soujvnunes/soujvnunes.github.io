7a302373310c86dd384d632a5759ecfadd79ec40
Victor <soujvnunes@gmail.com>
2024-03-29 13:58:11 -0300

    feat:HeaderNavTabs:remove tab group

--- src/containers/HeaderNavTabs.tsx
+++ src/containers/HeaderNavTabs.tsx
@@ -1,32 +1,26 @@
 import { useTranslation } from "react-i18next";
-import { Tab } from "@headlessui/react";
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
-import objectKeys from "helpers/objectKeys";
-import useTab from "hooks/useTab";
+import tabs from "config/tabs";
 
 export default function HeaderNavTabs() {
-  const tab = useTab();
   const [t] = useTranslation();
 
   return (
-    <Tab.List className="mr-auto space-x-2 lg:space-x-4">
-      {objectKeys(tab.group).map((_tab) => (
-        <Tab
-          key={_tab}
-          value={_tab}
-          onClick={tab.set}
-          className={twMerge(
-            classNames.text.color.secondary,
-            classNames.text.variant.title,
-            "inline-flex ui-selected:text-black/80 ui-selected:dark:text-white",
-            // TODO: remove on 2.1
-            "pointer-events-none",
-          )}
-        >
-          {t(`tabs.${_tab}`)}
-        </Tab>
-      ))}
-    </Tab.List>
+    <nav className="mr-auto space-x-2 lg:space-x-4">
+      <a
+        href={`/?tab=${tabs.contributions}`}
+        className={twMerge(
+          classNames.text.color.secondary,
+          classNames.text.variant.title,
+          "inline-flex ui-selected:text-black/80 ui-selected:dark:text-white",
+          // TODO: remove on 2.1
+          "pointer-events-none",
+        )}
+      >
+        {t(`tabs.${tabs.contributions}`)}
+      </a>
+      {/** TODO: implement About on 2.1 */}
+    </nav>
   );
 }
