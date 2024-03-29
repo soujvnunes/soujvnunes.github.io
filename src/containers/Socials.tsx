28ceea5844468c3885000f18ad82130396f8cb6b
Victor <soujvnunes@gmail.com>
2024-03-28 21:30:14 -0300

    feat:Socials:add block UI

--- src/containers/Socials.tsx
+++ src/containers/Socials.tsx
@@ -3,32 +3,61 @@ import { twMerge } from "tailwind-merge";
 import Button from "components/Button";
 import Icon from "components/Icon";
 
-type FooterSocialsProps = Pick<
-  React.ComponentPropsWithoutRef<"ul">,
-  "className"
->;
+interface SocialsProps
+  extends Pick<React.ComponentPropsWithoutRef<"ul">, "className"> {
+  inline?: boolean;
+}
 
-export default function FooterSocials({ className }: FooterSocialsProps) {
+export default function Socials({ className, inline }: SocialsProps) {
   const [t] = useTranslation();
 
   return (
-    <ul className={twMerge("space-x-2", className)}>
+    <ul
+      className={twMerge(
+        inline
+          ? "space-x-2"
+          : "inline-grid grid-cols-[104px_104px] grid-rows-[104px_104px] gap-2",
+        className,
+      )}
+    >
       {socials.map(({ name, href }) => {
         const title = t("socials_label", {
           name,
         });
+        const isBehance = !inline && name === "Behance";
+        const isGitHub = !inline && name === "GitHub";
+        const isLinkedIn = !inline && name === "LinkedIn";
 
         return (
-          <li className="inline-block" key={name}>
+          <li
+            className={twMerge(
+              inline && "inline-block",
+              isBehance && "text-right",
+              isGitHub && "text-left",
+              isLinkedIn && "flex items-end text-left",
+            )}
+            key={name}
+          >
             <Button
               as="a"
               variant="text"
               target="_blank"
               rel="noreferrer"
-              className="plausible-event-name=Visited+socials"
-              title={title}
+              className={twMerge(
+                "plausible-event-name=Visited+socials",
+                !inline && "inline-flex bg-gradient-to-tl",
+                isLinkedIn &&
+                  "h-10 w-10 from-[#0A66C2]/20 to-[#0A66C2]/10 text-[#0A66C2] dark:text-[#3782CD]",
+                isGitHub &&
+                  "h-16 w-16 from-[#333333]/20 to-[#333333]/10 text-[#333333] dark:from-white/10 dark:to-white/5 dark:text-white",
+                !inline &&
+                  name === "Medium" &&
+                  "h-[104px] w-[104px] from-[#aaaaaa]/20 to-[#aaaaaa]/10 text-black/80 dark:from-gray-50/10 dark:to-gray-100/5 dark:text-gray-200",
+                isBehance &&
+                  "h-[88px] w-[88px] from-[#0057FF]/20 to-[#0057FF]/10 text-[#0057FF] dark:text-[#2E76FF]",
+              )}
               href={href}
-              aria-label={title}
+              title={title}
             >
               <Icon name={name} title={title} />
             </Button>
@@ -45,16 +74,15 @@ const socials = [
     name: "Medium",
   },
   {
-    href: "https://github.com/soujvnunes",
-    name: "GitHub",
-  },
-  {
     href: "https://www.linkedin.com/in/soujvnunes/",
     name: "LinkedIn",
   },
-
   {
     href: "https://www.behance.net/soujvnunes",
     name: "Behance",
+  },
+  {
+    href: "https://github.com/soujvnunes",
+    name: "GitHub",
   },
 ] as const;
