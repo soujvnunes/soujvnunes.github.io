0def42835e232b6c69903ceb9525cc7667b00897
Victor <soujvnunes@gmail.com>
2024-04-03 19:09:56 -0300

    feat:App:implement store provider and snackbar container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { Provider } from "react-redux";
+import store from "config/store";
 import Contributions from "containers/Contributions";
 import Footer from "containers/Footer";
 import Header from "containers/Header";
 import HeaderIntro from "containers/HeaderIntro";
 import HeaderNav from "containers/HeaderNav";
 import Main from "containers/Main";
+import Snackbars from "containers/Snackbars";
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
+      <Snackbars />
       <Header>
         <HeaderIntro />
         <HeaderNav />
@@ -21,6 +25,6 @@ export default function App() {
          */}
       </Main>
       <Footer />
-    </>
+    </Provider>
   );
 }
