fd2d852f2d3bb77a1b6287b220e614ab32525556
Victor <soujvnunes@gmail.com>
2024-04-03 19:10:32 -0300

    feat:Footer:show web analytics snackbar on mount

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,9 +1,16 @@
 import { twMerge } from "tailwind-merge";
 import classNames from "consts/classNames";
+import useSnackbar from "hooks/useSnackbar";
 import FooterCopyright from "./FooterCopyright";
 import FooterSocials from "./FooterSocials";
 
 export default function Footer() {
+  useSnackbar({
+    title: "📊 Web Analytics",
+    children:
+      "This website uses a privacy-friendly third-party web analytics services to track visitors by country, device and browser; so it can be enhanced overtime.",
+  });
+
   return (
     <footer className={twMerge(classNames.container.root, "text-center")}>
       <FooterSocials />
