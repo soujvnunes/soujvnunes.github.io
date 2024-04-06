b3ef69cb5ae53dd216b2b58f1593b88f9949b632
Victor <soujvnunes@gmail.com>
2024-04-06 13:53:38 -0300

    refactor:snackbars:adjust to receive just a stack

--- src/stores/snackbars.ts
+++ src/stores/snackbars.ts
@@ -1,24 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { type PayloadAction } from "@reduxjs/toolkit";
 
-export type Snackbar = React.PropsWithChildren<{
-  title?: string;
-}>;
-interface _Snackbar extends Snackbar {
+interface Snackbar {
   id: string;
+  height: number;
 }
 
-const initialState: _Snackbar[] = [];
+const initialState: Snackbar[] = [];
 
 export default createSlice({
   name: "snackbars",
   initialState,
   reducers: {
-    show: (state, action: PayloadAction<_Snackbar>) => {
-      state.push(action.payload);
-    },
-    hide: (state, action: PayloadAction<_Snackbar["id"]>) => {
-      return state.filter(({ id }) => action.payload !== id);
-    },
+    add: (state, action: PayloadAction<Snackbar>) => [
+      action.payload,
+      ...state.filter(({ id }) => action.payload.id !== id),
+    ],
+    remove: (state, action: PayloadAction<Snackbar["id"]>) =>
+      state.filter(({ id }) => id !== action.payload),
   },
 });
