27402453f06393f534c36f118bf5fc5be74aca7f
Victor <soujvnunes@gmail.com>
2024-04-06 14:02:51 -0300

    feat:HeaderIntroMemoji:adjust snackbar

--- src/containers/HeaderIntroMemoji.tsx
+++ src/containers/HeaderIntroMemoji.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import { PickFrom } from "types/PickFrom";
