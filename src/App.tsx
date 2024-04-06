910ca1e2a034f12d370679517a461c0aea323690
Victor <soujvnunes@gmail.com>
2024-04-06 14:03:05 -0300

    fix:App:remove unused module

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import Header from "containers/Header";
 import HeaderIntro from "containers/HeaderIntro";
 import HeaderNav from "containers/HeaderNav";
 import Main from "containers/Main";
-import Snackbars from "containers/Snackbars";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Snackbars />
       <Header>
         <HeaderIntro />
         <HeaderNav />
