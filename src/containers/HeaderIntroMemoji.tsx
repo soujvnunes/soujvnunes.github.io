daaf77fa986204f6766743f8eac2dc39fa410181
Victor <soujvnunes@gmail.com>
2024-04-07 15:16:16 -0300

    feat:HeaderIntroMemoji:adjust supports mov detection

--- src/containers/HeaderIntroMemoji.tsx
+++ src/containers/HeaderIntroMemoji.tsx
@@ -1,34 +1,24 @@
-import { useCallback, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
-import hasWebKit from "helpers/hasWebKit";
+import getSupportsMov from "helpers/getSupportsMov";
 import { PickFrom } from "types/PickFrom";
 import HeaderIntroMemojiFallback from "./HeaderIntroMemojiFallback";
 import HeaderIntroMemojiMov from "./HeaderIntroMemojiMov";
-import Snackbars from "./Snackbars";
 
-type HeaderMemojiProps = PickFrom<"span", "className">;
+type HeaderIntroMemojiProps = PickFrom<"span", "className">;
 
-export default function HeaderIntroMemoji({ className }: HeaderMemojiProps) {
-  const [t] = useTranslation();
-  const [show, setShow] = useState(false);
-  const handleShow = useCallback((params: boolean) => {
-    setShow(params);
-  }, []);
-
+export default function HeaderIntroMemoji({
+  className,
+}: HeaderIntroMemojiProps) {
   return (
     <>
-      <Snackbars show={show} title={t("reduced_resources.title")}>
-        {t("reduced_resources.description")}
-      </Snackbars>
       <span
         className={twMerge(
           "inline-flex h-32 w-32 items-center justify-center overflow-hidden",
           className,
         )}
       >
-        {hasWebKit ? (
-          <HeaderIntroMemojiMov onPlaying={handleShow} />
+        {getSupportsMov() ? (
+          <HeaderIntroMemojiMov />
         ) : (
           <HeaderIntroMemojiFallback />
         )}
