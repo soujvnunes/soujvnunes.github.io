cf376c577e2f2a90bd9b71243d9600c156ec1bfd
Victor <soujvnunes@gmail.com>
2024-04-16 16:35:29 -0300

    feat:root:remove assertion

--- src/config/root.ts
+++ src/config/root.ts
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-export const container = document.getElementById("root")!;
+export const container = document.getElementById("root");
 
-export default createRoot(container);
+export default container ? createRoot(container) : null;
